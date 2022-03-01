eec4a660294c1a2c669f550e41aea2772343fbf9
Joe Webster <joseph.scott.webster@gmail.com>
2022-02-28 19:37:07 -0500

    Create a link to sort the file list

--- index.rb
+++ index.rb
@@ -7,5 +7,6 @@ require 'tilt/erubis'
 get '/' do
   file_paths = Dir.glob('public/*')
   @files = file_paths.map { |file| File.basename(file) }.sort
+  @files.reverse! if params[:sort] == 'desc'
   erb :list
 end
